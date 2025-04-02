61b340e752e8555e5b8763888e61ac3d40599c01
shivam-9188 <shivamkhurana740@gmail.com>
2025-04-02 13:45:42 +0530

    Update main.c.cpp

--- main.c.cpp
+++ main.c.cpp
@@ -28,10 +28,3 @@ int main() {
 
     return 0;
 }
-
-
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
